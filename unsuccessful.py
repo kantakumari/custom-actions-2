430568bb5b2086ee1a68d992cbf93566660de5a8
kantakumari <kanta.kumari@publicissapient.com>
2023-02-13 16:05:36 +0530

    v1.1.4

--- unsuccessful.py
+++ unsuccessful.py
@@ -7,17 +7,10 @@ port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
-githubjob= sys.argv[1]
-githubrepo = sys.argv[2]
-githubwork = sys.argv[3]
-subject = '{p1} job of {p2} had failed'
-subject2 = subject.format(p1=githubjob, p2=githubrepo)
-body1 = '{c1} job in worflow {c3} of {c2} had Failed'
-body2 = body1.format(c1=githubjob, c3=githubwork, c2=githubrepo)
 message = f"""\
-Subject: {subject2}
+Subject: Github Custom Notification 
 
-{body2}
+Hello World from github custom notification
 """
 
 context = ssl.create_default_context()
